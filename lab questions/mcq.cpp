2b77714a2b009cf9f54e1df5a3235e482f00336d
mdfarhanali3 <91973246+mdfarhanali3@users.noreply.github.com>
2023-11-28 01:44:45 +0530

    Add files via upload

---
+++ lab questions/mcq.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ 
+class point{
+    public:
+    point(){
+        cout << "Constructor called";
+    }
+};
+int main()
+{
+    point t1;
+    return 0;
+}
